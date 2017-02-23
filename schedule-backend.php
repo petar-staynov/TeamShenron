d9619e21b3f7073457d65e814bb8f4349c48a81e
petar-staynov <petar.staynov@gmail.com>
2017-02-23 03:39:44 +0200

    Programa simple frontend

---
+++ schedule-backend.php
@@ -0,0 +1,8 @@
+<?php
+include "schedule-frontend.php";
+
+if(isset($_GET["submit-monday"])){
+    echo "SUBMIT MONDAY";
+}
+
+//TODO parse/upload schedules from/to database
---
+++ schedule-frontend.php
@@ -0,0 +1,223 @@
+<style>
+    input[type="submit"] {
+        width: 100%;
+    }
+</style>
+
+<form method="GET">
+    <table border="thick">
+        <th>Понеделник<th>Вторник</th>
+        <th>Сряда</th>
+        <th>Четвъртък</th>
+        <th>Петък</th>
+        <th>Събота</th>
+        <th>Неделя</th>
+        <tr id="schedule-row-1">
+            <td>
+                <input type="text" name="monday-1" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-1"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-1"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-1"\>
+            </td>
+            <td>
+                <input type="text" name="friday-1"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-1"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-1"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-2">
+            <td>
+                <input type="text" name="text" id="monday-2" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-2"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-2"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-2"\>
+            </td>
+            <td>
+                <input type="text" name="friday-2"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-2"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-2"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-3">
+            <td>
+                <input type="text" name="text" id="monday-3" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-3"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-3"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-3"\>
+            </td>
+            <td>
+                <input type="text" name="friday-3"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-3"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-3"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-4">
+            <td>
+                <input type="text" name="text" id="monday-4" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-4"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-4"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-4"\>
+            </td>
+            <td>
+                <input type="text" name="friday-4"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-4"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-4"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-5">
+            <td>
+                <input type="text" name="text" id="monday-5" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-5"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-5"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-5"\>
+            </td>
+            <td>
+                <input type="text" name="friday-5"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-5"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-5"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-6">
+            <td>
+                <input type="text" name="text" id="monday-6" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-6"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-6"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-6"\>
+            </td>
+            <td>
+                <input type="text" name="friday-6"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-6"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-6"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-7">
+            <td>
+                <input type="text" name="text" id="monday-7" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-7"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-7"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-7"\>
+            </td>
+            <td>
+                <input type="text" name="friday-7"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-7"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-7"\>
+            </td>
+        </tr>
+        <tr id="schedule-row-8">
+            <td>
+                <input type="text" name="text" id="monday-8" \>
+            </td>
+            <td>
+                <input type="text" name="tuesday-8"\>
+            </td>
+            <td>
+                <input type="text" name="wednesday-8"\>
+            </td>
+            <td>
+                <input type="text" name="thursday-8"\>
+            </td>
+            <td>
+                <input type="text" name="friday-8"\>
+            </td>
+            <td>
+                <input type="text" name="saturday-8"\>
+            </td>
+            <td>
+                <input type="text" name="sunday-8"\>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <input type="submit" name="submit-monday" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-tuesday" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-wednesay" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-thursday" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-friday" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-saturday" value="Submit"\>
+            </td>
+            <td>
+                <input type="submit" name="submit-sunday" value="Submit"\>
+            </td>
+        </tr>
+    </table>
+</form>

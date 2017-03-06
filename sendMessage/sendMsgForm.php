a5cf9f936d9fe175df5743f5043ce39cba9c34d0
YovoMitev <iovo.mitev@abv.bg>
2017-03-06 08:27:54 +0200

    quick fix

--- sendMessage/sendMsgForm.php
+++ sendMessage/sendMsgForm.php
@@ -3,9 +3,9 @@ $currentUser = "Мариика"
 ?>
 <form>
     <fieldset>
-        <legend>Изпращане на съобщение</legend>
-        <span class="number">От</span><input type="text" name="field1" value="<?=$currentUser ?>" name="sender" disabled>
-        <span class="number">До</span><input type="email" name="field2" placeholder="Изпрати до *" name="recipient">
+        <legend class="title">Изпращане на съобщение</legend>
+        <span class="pop">От</span><input type="text" name="field1" value="<?=$currentUser ?>" name="sender" disabled>
+        <span class="pop">До</span><input type="text" name="field2" placeholder="Изпрати до *" name="recipient">
         <span><i class="fa fa-comment fa-2x"></i></span>
         <textarea name="field3" placeholder="Съобщение" class="message" name="content"></textarea>
     </fieldset>
